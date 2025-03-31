e80a7bdd752c0a9566baeb5ac6404fed12cd2a0d
Sharkpb8 <sharkpb8@gmail.com>
2025-03-31 17:58:43 +0200

    Added: gbr model

--- backend/api/predict.py
+++ backend/api/predict.py
@@ -1,12 +1,11 @@
 import pickle
 import json
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 def Predict(house):
-    with open("./backend/training/SavedModels/RandomForestRegression.dat","rb") as f:
+    with open("./backend/training/SavedModels/GradientBoostingRegressor.dat","rb") as f:
         model = pickle.load(f)
-    with open("./backend/training/SavedModels/RandomForestRegression.json","r") as f:
+    with open("./backend/training/SavedModels/GradientBoostingRegressor.json","r") as f:
         modelAttr = json.load(f)
 
     data = []
@@ -14,8 +13,6 @@ def Predict(house):
         if key in modelAttr:
             data.append(val)
     pd_data = pd.DataFrame([data],columns=modelAttr)
-    scaler = StandardScaler()
-    # pd_data = scaler.fit_transform(pd_data)
     result = model.predict(pd_data)
     result = int(result)
     formatedResult = "{:,}".format(result).replace(",", " ")
